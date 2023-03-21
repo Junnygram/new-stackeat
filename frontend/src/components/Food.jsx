9eb03e7e916687fc208393b5ef1c8d2a9e8e6c38
Olaleye <90139191+Junnygram@users.noreply.github.com>
2023-03-21 14:33:52 +0100

    made the filter button functional

--- frontend/src/components/Food.jsx
+++ frontend/src/components/Food.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
 import Product from "./Product";
 import { Helmet } from "react-helmet-async";
@@ -20,6 +20,7 @@ const reducer = (state, action) => {
 };
 
 function Food() {
+  const [allProducts, setAllProducts] = useState();
   const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
@@ -31,6 +32,7 @@ function Food() {
       try {
         const result = await axios.get("/api/products");
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        setAllProducts(result.data);
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
@@ -38,6 +40,21 @@ function Food() {
     fetchData();
   }, []);
   console.log(products);
+
+  const filterFoods = (filter, price) => {
+    if (filter === "") {
+      setAllProducts(products);
+      return;
+    }
+    if (price === true) {
+      const priceProduct = products.filter((x) => x.price === filter);
+      setAllProducts(priceProduct);
+
+      return;
+    }
+    const returnProducts = products.filter((x) => x.category === filter);
+    setAllProducts(returnProducts);
+  };
 
   // // //filter
 
@@ -77,31 +94,31 @@ function Food() {
           <p className="font-bold text-gray-700">Filter Type</p>
           <div className="flex justfiy-between flex-wrap">
             <button
-              // onClick={() => setFoods(products)}
+              onClick={() => filterFoods("")}
               className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white hover:italic"
             >
               All
             </button>
             <button
-              // onClick={() => filterType("burger")}
+              onClick={() => filterFoods("burger")}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               Burgers
             </button>
             <button
-              // onClick={() => filterType("pizza")}
+              onClick={() => filterFoods("pizza")}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               Pizza
             </button>
             <button
-              // onClick={() => filterType("salad")}
+              onClick={() => filterFoods("salad")}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               Salads
             </button>
             <button
-              // onClick={() => filterType("chicken")}
+              onClick={() => filterFoods("chicken")}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               Chicken
@@ -114,25 +131,25 @@ function Food() {
           <p className="font-bold text-gray-700">Filter Price</p>
           <div className="flex justify-between max-w-[390px] w-full">
             <button
-              // onClick={() => setfilterPrice("$10")}
+              onClick={() => filterFoods(10, true)}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               $10
             </button>
             <button
-              // onClick={() => setfilterPrice("$20")}
+              onClick={() => filterFoods(20, true)}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               $20
             </button>
             <button
-              // onClick={() => setfilterPrice("$50")}
+              onClick={() => filterFoods(50, true)}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               $50
             </button>
             <button
-              // onClick={() => setfilterPrice("$100")}
+              onClick={() => filterFoods(100, true)}
               className="hover:italic m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
             >
               $100
@@ -148,7 +165,7 @@ function Food() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product, index) => (
+          allProducts.map((product, index) => (
             <Product key={index} product={product}></Product>
           ))
         )}
